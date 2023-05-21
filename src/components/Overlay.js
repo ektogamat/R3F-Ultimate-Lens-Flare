3d52c1e991400b3199c16b59e97fc8d85df6139d
Anderson Mancini <anderson@neotix.com.br>
2023-05-21 02:15:51 -0300

    Added the link to my website

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,7 +1,7 @@
 export default function Overlay() {
   return (
     <>
-      <h2>ANDERSON MANCINI.DEV</h2>
+      <h2><a href="https://andersonmancini.dev" target="_blank">ANDERSON MANCINI.DEV</a></h2>
 
       <div className="container">
         <h3>REACT THREE FIBER ULTIMATE LENS FLARE</h3>
